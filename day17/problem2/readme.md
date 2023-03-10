3010dba578122de37892792d1713e0490cd2a0db
billcates <m.billcates@gmail.com>
2023-03-10 09:56:02 +0530

    day 17 problem 2
    day 17 problem 2

---
+++ day17/problem2/readme.md
@@ -0,0 +1,11 @@
+#sql-prep
+##day17
+
+2nd most recent activity
+
+Code Ninja Problem
+
+Link:
+https://www.codingninjas.com/codestudio/problems/get-the-second-most-recent-activity_2196170?leftPanelTab=0
+
+Write a sql query to return the 2 nd most recent activity and if there are less than 2 activities return the 1 st activity.
---
+++ day17/problem2/solution.sql
@@ -0,0 +1,19 @@
+with cte as(
+    select *,
+    row_number() over(partition by username order by startdate) as rk
+    ,count(activity) over(partition by username) ct
+    from useractivity 
+)
+select 
+    username,
+    activity,
+    startdate,
+    enddate
+from cte where rk=2
+union all 
+select 
+    username,
+    activity,
+    startdate,
+    enddate
+from cte where ct<2
