a8632c9a766016465114250b29f58fd540ceaac9
billcates <56115142+billcates@users.noreply.github.com>
2023-04-29 08:11:24 +0530

    postgres solution added

--- day64/part2/solution.sql
+++ day64/part2/solution.sql
@@ -15,4 +15,16 @@ with cte as(
 )
 select * 
 from final
-order by value desc
+order by value desc;
+
+
+--Postrgres Solution
+with cte as(
+select unnest(string_to_array(contents,' ')) as words
+from google_file_store
+)
+select words,count(*) as ct
+from cte
+where words='bull' or words='bear'
+group by 1
+order by 1 desc
